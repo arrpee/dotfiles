b54fbcd24e331b9366b611202e89d7d75359c447
Raaj Patil <raaj.2501@gmail.com>
2020-04-14 15:31:49 +0530

    Fix accent colour usage in borders

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -14,28 +14,30 @@ theme.icon_path = awful.util.getdir("config") .. "/icons/"
 theme.default_dir = require("awful.util").get_themes_dir() .. "default"
 
 theme.fg_normal = xrdb.foreground
-theme.fg_focus = xrdb.color1
-theme.fg_urgent = "#CC9393"
+theme.fg_focus = xrdb.color4
+theme.border_focus = theme.fg_focus
 theme.taglist_fg_focus = theme.fg_focus
+theme.taglist_shape_border_color_focus = theme.fg_focus
+theme.tasklist_shape_border_color_focus = theme.fg_focus
 theme.notification_fg = theme.fg_normal
 
 theme.bg_normal = xrdb.background
 theme.bg_focus = xrdb.color0
-theme.bg_urgent = "#006B8E"
+theme.border_normal = theme.bg_normal
 theme.taglist_bg_focus = theme.bg_focus
+theme.taglist_shape_border_color = theme.bg_normal
+theme.tasklist_shape_border_color = theme.bg_normal
 theme.notification_bg = theme.bg_normal
 
-theme.border_width = dpi(2)
-theme.border_normal = xrdb.background
-theme.border_focus = xrdb.background
-theme.taglist_shape_border_width = dpi(0)
-theme.tasklist_shape_border_width = dpi(0)
-
+theme.tasklist_disable_icon = true
 theme.useless_gap = dpi(8)
+theme.border_width = dpi(1)
+theme.taglist_shape_border_width = dpi(1)
+theme.tasklist_shape_border_width = dpi(1)
 
-theme.notification_border_width = dpi(2)
+theme.notification_border_width = theme.border_width
 theme.notification_border_color = theme.fg_normal
-theme.notification_margin = dpi(8)
+theme.notification_margin = theme.useless_gap
 theme.notification_border_width_critical = "#FF0000"
 
 return theme
