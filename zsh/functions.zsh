f1a3dbe5aa8b3c152b1f343615e3f4ac62e72497
Raaj Patil <raaj.2501@gmail.com>
2020-04-27 00:25:30 +0530

    Add the cf function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,9 +1,24 @@
 # Auto LS
-function chpwd () { ls -A }
+function chpwd() { ls -A; }
 
 # Mkdir and CD automatically
 function mkcd() { mkdir "$1" && cd "$1"; }
 
 # Python virtual environments
-function pyve() { python3 -m venv venv }
-function pyva() { source ./venv/bin/activate }
+function pyve() { python3 -m venv venv; }
+function pyva() { source ./venv/bin/activate; }
+
+# Locate file/folder and cd to it
+function cf() {
+  local file
+
+  file="$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf --read0 -0 -1)"
+
+  if [[ -n $file ]]; then
+    if [[ -d $file ]]; then
+      cd -- $file
+    else
+      cd -- ${file:h}
+    fi
+  fi
+}
