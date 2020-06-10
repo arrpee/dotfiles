8590fd33d5f3ef2ce53e48f00467dd2cad557e25
Raaj Patil <raaj.2501@gmail.com>
2020-06-10 16:13:01 +0530

    Remove dead code

--- awesome/widgets/systray.lua
+++ awesome/widgets/systray.lua
@@ -1,24 +1,9 @@
 -- Imports
 local wibox = require("wibox")
-local toggle_widget = require("utils.toggle_widget")
 
 -- Systray Widget
 local systray = wibox.widget.systray(true)
-systray.visible = true
 systray.set_base_size(28)
 systray.set_horizontal(true)
-local systray_widget =
-    create_toggle_widget(
-    false,
-    "   ",
-    "   ",
-    function()
-        systray.visible = true
-    end,
-    function()
-        systray.visible = false
-    end,
-    wibox.widget.textbox
-)
 
 return systray
