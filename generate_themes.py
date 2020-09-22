f33c84fad653f6cc5565751b876041979b411817
Raaj Patil <raaj.2501@gmail.com>
2020-09-22 12:32:22 +0530

    Clean up theme generation script

--- generate_themes.py
+++ generate_themes.py
@@ -15,18 +15,16 @@ def hex_to_rgb(hex_col):
     return lst
 
 
-def find_jinja_dirs():
+if __name__ == "__main__":
+
+    # Find paths for templates
     template_paths = set()
     for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
         for file in files:
             if file.endswith(".jinja"):
                 template_paths.add(root)
 
-    return template_paths
-
-
-def process_templates(template_paths):
-
+    # Read xrdb to get mappings
     xresources = subprocess.check_output(["xrdb", "-query", "-all"]).decode("utf-8")
     xresources_dict = {
         y[0]: y[1]
@@ -45,6 +43,7 @@ def process_templates(template_paths):
         {f"xr_{x}": xresources_dict[f"*{x}"] for x in [f"color{i}" for i in range(16)]}
     )
 
+    # Patch templates
     for i in template_paths:
         env = Environment(loader=FileSystemLoader(searchpath=i))
         env.globals["hex_to_rgb"] = hex_to_rgb
@@ -56,8 +55,3 @@ def process_templates(template_paths):
             template = env.get_template(j)
             with open(os.path.join(i, j[:-6]), "w") as f:
                 f.write(template.render(mappings=mappings))
-
-
-if __name__ == "__main__":
-    template_paths = find_jinja_dirs()
-    process_templates(template_paths)
